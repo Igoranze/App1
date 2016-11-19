ddfb84512f9835b50552cb8b0b7da6363b31657d
Velders Software Development <jeroen@vsdev.nl>
2016-11-19 14:32:14 +0100

    refactored some of the code to implement de Code/Categories

---
+++ Dominos/Constants.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominos
+{
+    public class Constants
+    {
+        public class Code {
+            public class Food
+            {
+                public string pizza = "Menu.Pizza.Promo";
+
+                //public string Pizza { get { return pizza; }}
+
+            }
+            public class Drinks
+            {
+                public enum Codes
+                {
+                    //Desserts = "Menu.Drink.Desserts"
+                }
+            }
+        }
+    }
+}
